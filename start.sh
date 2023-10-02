08c82f3c94516b16e29765aee919bbf9098e0cf9
Alberto Leiva Popper <ydahhrk@gmail.com>
2023-10-01 22:40:30 -0600

    Update startup script

--- start.sh
+++ start.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
 
-make # No need to install
-sudo insmod joolif.ko
+# To compile, simply run `make`. No need for configure.
+# You need your kernel headers & Kbuild & stuff.
 
-sudo ip addr add 1.2.3.4/24 dev siit0
-sudo ip addr add 64:ff9b::0102:0306/96 dev siit0
+set -x
 
+sudo insmod mod/joolif.ko
+
+sudo sysctl -w net.ipv4.conf.all.forwarding=1 > /dev/null
+sudo sysctl -w net.ipv6.conf.all.forwarding=1 > /dev/null
+
+sudo ip addr flush dev siit0 scope global
+sudo ip addr add 2001:db8:1c0:2:21::/40 dev siit0 # 192.0.2.33
+sudo ip addr add 198.51.100.2/5 dev siit0 # 2001:db8:1c6:3364:2::
 sudo ip link set siit0 up
 
-# Test ping: ping6 64:ff9b::1.2.3.4
+sudo usr/joolif siit0 pool6 2001:db8:100::/40
+sudo usr/joolif siit0 pool6791v4 198.51.100.1
+sudo usr/joolif siit0 pool6791v6 2001:db8:1c0:2:1::
 
-# Remove the module: sudo rmmod joolif
+# Not sure if this is still relevant
+sudo sysctl -w net.ipv6.auto_flowlabels=0 > /dev/null
+
+# Only one interface can exist at a time, for now.
+#
+# Sample pings to yourself through xlator:
+#	ping 2001:db8:1c6:3364:2::
+#	ping 192.0.2.33
+#
+# End:
+#	sudo rmmod joolif
 
